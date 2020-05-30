264c91e89ee74fb0a7e8a0e162d90e19d26112e9
diego_neri <diego@diegoneri.com>
2020-05-29 22:46:24 -0300

    Finaliza o exercício GrausCF

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+/*
+[console: GrausCF] Converta uma temperatura digitada pelo usuário em °C para °F.
+
+°F = °C × 1,8 + 32
+
+Ref.: https://pt.wikipedia.org/wiki/Celsius
+
+°C = 0
+0°C equivalem a 32°F
+*/
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grausCelsius, grausFahenreit;
+            
+            Console.WriteLine("Informe um valor de temperatura em Celsius (°C)");
+
+            grausCelsius = Convert.ToDouble(Console.ReadLine());
+            grausFahenreit = grausCelsius * 1.8 + 32;
+
+            Console.WriteLine($"{grausCelsius:N1}°C equivalem a {grausFahenreit:N1}°F");
+        }
+    }
+}
